7781a863fcf9694ac76cff4c04f8360c0d90a9d7
Filip Czaplicki <github@starsep.com>
2025-05-10 19:03:24 +0200

    healthchecks

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from data.GeoJSONSaver import GeoJSONSaver
 from tczew.TczewGTFSGenerator import GTFSTczew
+from starsep_utils.healthchecks import healthchecks
+
 
 if __name__ == "__main__":
+    healthchecks("/start")
     gtfs = GTFSTczew()
     gtfs.generate()
     GeoJSONSaver().save(gtfs.operatorData)
     # gtfs.showTrips()
+    healthchecks()
